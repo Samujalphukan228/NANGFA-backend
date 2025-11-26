67429f5994df44860a8ebba38d44f159baf68ea2
Your Name <your.email@example.com>
2025-11-27 02:23:03 +0530

    6

--- backend/src/utils/env.js
+++ backend/src/utils/env.js
@@ -9,9 +9,6 @@ const env = {
     jwtSecret: process.env.JWT_SECRET,
     adminEmail: process.env.ADMIN_EMAIL,
     employfrontend: process.env.FRONTEND_URL,
-    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    cloudApiKey: process.env.CLOUDINARY_API_KEY,
-    cloudApiSecret: process.env.CLOUDINARY_API_SECRET,
 }
 
 export default env 
